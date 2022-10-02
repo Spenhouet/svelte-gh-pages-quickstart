eb8bcda9f5ad20e7802307108ca5892bf470cd63
Sebastian Penhouet <s.penhouet@outlook.de>
2022-10-02 13:47:50 +0200

    Migrate svelte version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import SvelteSeo from 'svelte-seo';
+</script>
+
+<SvelteSeo
+	title="GitHub pages with Svelte"
+	description="Quickstart project to develop and deploy a static Svelte page to GitHub pages."
+/>
+
+<main>
+	<h1 class="text-3xl font-bold">Welcome to Svelte GitHub Pages Quickstart</h1>
+	<p>Quickstart project to develop and deploy a static Svelte page to GitHub pages.</p>
+	<p>
+		Read the <a href="https://github.com/Spenhouet/svelte-gh-pages-quickstart#readme">README</a> to get
+		started.
+	</p>
+</main>
